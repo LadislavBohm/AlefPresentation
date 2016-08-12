0a82652cf1d97894fe9b27071705363781eb0399
Ladislav Böhm <bohmladislav@gmail.com>
2016-08-12 09:22:23 +0200

    + client + ToString override

--- AlefPresentation.Client/Program.cs
+++ AlefPresentation.Client/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AlefPresentation.Model;
+using RestSharp;
 
 namespace AlefPresentation.Client
 {
@@ -10,6 +12,23 @@ namespace AlefPresentation.Client
     {
         static void Main(string[] args)
         {
+            var client = new RestClient("http://localhost:2883/api");
+            var getRequest = new RestRequest("lecturer",Method.GET);
+
+            var result = client.Execute<List<Lecturer>>(getRequest);
+
+            foreach (var lecturer in result.Data)
+            {
+                Console.WriteLine(lecturer);
+            }
+
+
+            var postRequest = new RestRequest("lecturer",Method.PUT);
+
+
+            var postResult = client.Execute(postRequest);
+
+            Console.ReadLine();
         }
     }
 }
